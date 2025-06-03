2450607b91828b5fbf0384cabfee531478f9dc5d
Isnu Nasrudin <isnunas@gmail.com>
2025-06-03 23:52:18 +0700

    allow panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
@@ -39,7 +41,7 @@ class User extends Authenticatable
         return $this->hasMany(SessionPicture::class, 'user_id', 'id');
     }
 
-    public function canAccessPanel()
+    public function canAccessPanel(Panel $panel) : bool
     {
         return true;
     }
